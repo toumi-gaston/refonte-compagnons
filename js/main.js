8bace1245c22f56b612628d6384d6626e630e6e2
Bryan Jobin <Bryanjobin@gmail.com>
2023-08-25 09:43:57 -0400

    update du header

--- js/main.js
+++ js/main.js
@@ -1,49 +1,15 @@
-const mobileHeaderBtns = document.querySelectorAll('.m_header_container_button_primary');
-const mobileHeaderBtnsLists = document.querySelectorAll('.m_header_container_button_secondary');
-const mobileHeaderMenu = document.querySelector('.m_header_container');
-const mobileHeaderMenuBtn = document.querySelector('.m_header_top_hamburger');
-const mobileHeaderMenuBtnIcon = document.querySelector('.m_header_top_hamburger_icon');
-const mobileMain = document.querySelector('main');
-const mobileFooter = document.querySelector('footer');
-
-mobileHeaderMenuBtn.addEventListener('click', () => {
-    if (mobileHeaderMenuBtnIcon.textContent === 'close') {
-        mobileHeaderMenuBtnIcon.textContent = 'menu';
-        mobileHeaderMenu.style.display = 'none';
-        mobileMain.style.display = 'block';
-        mobileFooter.style.display = 'block';
-    } else {
-        mobileHeaderMenuBtnIcon.textContent = 'close';
-        mobileHeaderMenu.style.display = 'flex';
-        mobileMain.style.display = 'none';
-        mobileFooter.style.display = 'none';
-    }
-})
-
-mobileHeaderBtns.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        const isHidden = mobileHeaderBtnsLists[index].style.display === 'none' || mobileHeaderBtnsLists[index].style.display === '';
-
-        if (isHidden) {
-            mobileHeaderBtnsLists[index].style.display = 'flex';
-            button.querySelector('.m_header_container_button_primary_right .material-symbols-outlined').textContent = 'expand_less';
-        } else {
-            mobileHeaderBtnsLists[index].style.display = 'none';
-            button.querySelector('.m_header_container_button_primary_right .material-symbols-outlined').textContent = 'expand_more';
-        }
-    });
-});
-
 // ACCORDION //
-
 const accordion = document.querySelector('.simpliquer_menterie_accordion');
-const panel = document.querySelector('.simpliquer_menterie_accordion_panel');
-const icon = document.querySelector('.material-symbols-outlined');
-const chevronUp = "expand_less";
-const chevronDown = "expand_more";
-let currentIcon = chevronUp;
 
-accordion.addEventListener('click', () =>{
+if (accordion) {
+    const panel = document.querySelector('.simpliquer_menterie_accordion_panel');
+    const icon = document.querySelector('.material-symbols-outlined');
+    const chevronUp = "expand_less";
+    const chevronDown = "expand_more";
+    let currentIcon = chevronUp;
+
+
+    accordion.addEventListener('click', () =>{
     if (panel.style.display === "block" && currentIcon === chevronUp){
         panel.style.display = "none";
         icon.innerHTML = currentIcon;
@@ -54,3 +20,6 @@ accordion.addEventListener('click', () =>{
         currentIcon = chevronUp;
     }
 })
+}
+
+
